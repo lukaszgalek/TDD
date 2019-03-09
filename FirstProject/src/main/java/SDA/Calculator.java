7cef04cb02842ba3de047800d20d6b910521750e
Lukasz Galek <lukasz.galek@o2.pl>
2019-03-09 11:03:06 +0100

    params

--- FirstProject/src/main/java/SDA/Calculator.java
+++ FirstProject/src/main/java/SDA/Calculator.java
@@ -10,8 +10,8 @@ public class Calculator {
 //    }
 
 
-    public int add (int liczba1, int liczba2){
-        return liczba1 + liczba2;
+    public int add (int a,int b){
+        return a + b;
     }
 
     public int roznica (int a , int b ){
--- FirstProject/src/test/java/SDA/CalculatorTest.java
+++ FirstProject/src/test/java/SDA/CalculatorTest.java
@@ -1,44 +1,53 @@
 package SDA;
 
+import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
 
 public class CalculatorTest {
 
     private Calculator calculator;
-
+//    private Integer value;
+//
+//    public CalculatorTest(Integer value){
+//        this.value = value;
+//    }
 
     @Before
-    public void setUp () throws Exception {
+    public void setUp() throws Exception {
         this.calculator = new Calculator();
     }
 
     @Test
-    @Parameters({"3,2","4,4","8,2"})
-    public void testAdding(int a, int b)throws Exception{
-        int result = calculator.add(a,b);
-        Assert.assertEquals(a+b,result);
+    @Parameters({"3,2,", "4,4", "8,2"})
+    public void testAdding(int a, int b) throws Exception {
+        Assert.assertEquals(a + b, calculator.add(a, b));
     }
 
     @Test
-    public void testRoznica() {
-        int result = calculator.roznica(3,1);
-        Assert.assertEquals(,result);
+    @Parameters({"2,2,0", "4,2,2"})
+    public void testRoznica(int a, int b, int c) throws Exception {
+
+        Assert.assertEquals(c, calculator.roznica(a, b));
     }
 
     @Test
     public void testMnozenie() {
-        int result = calculator.mnozenie(2,3);
-        Assert.assertEquals(6,result);
+        int result = calculator.mnozenie(2, 3);
+        Assert.assertEquals(6, result);
     }
-// dzieki rzutowaniiu na obiekt Double nie trzeba delty po przecinku
+
+    // dzieki rzutowaniiu na obiekt Double nie trzeba delty po przecinku
     @Test
     public void testDzielenie() {
 
-        Double result = calculator.dzielenie(4,2);
-        Assert.assertEquals(Double.valueOf(2.0),result);
+        Double result = calculator.dzielenie(4, 2);
+        Assert.assertEquals(Double.valueOf(2.0), result);
     }
 // miejsca po przecinku możńa określić
 //    @Test
